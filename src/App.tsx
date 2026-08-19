98cb2f00c57b2dae74ff4cf98cf47e2a30fd857f
FormOfShred <robynhamelryck@outlook.com>
2026-08-19 17:17:56 +0200

    [RESET] remove old implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,8 @@
-import { useEffect, useState } from "react"
-import NavigationPanel from "./components/NavigationPanel/NavigationPanel"
-import InfoSections from "./components/InfoSections/InfoSections"
 
 function App() {
-  const [activeSection, setActiveSection] = useState<string>("about")
-  const [bigScreen, setBigScreen] = useState<boolean>(window.innerWidth > 1200);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setBigScreen(window.innerWidth > 1200);
-    };
-
-    handleResize();
-
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
-
   return (
-    <div className="flex app">
-      {bigScreen && <NavigationPanel activeSection={activeSection} setActiveSection={setActiveSection} />}
-      <InfoSections activeSection={activeSection} bigScreen={bigScreen} />
+    <div>
+
     </div>
   )
 }
